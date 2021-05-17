974ee44f2e793b9fd3f7c2fb94adf36b328f6627
blumouse <50953717+blumouse@users.noreply.github.com>
2021-05-17 20:14:36 +0900

    배경 움직임, 튜토리얼 일부

---
+++ DOITGameDev/Assets/Scripts/Main_StartBtn.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Main_StartBtn : MonoBehaviour
+{
+    Image curtain;
+    Color setAlpha;
+
+    public void GameStart()
+    {
+        curtain = GameObject.Find("Curtain").GetComponent<Image>();
+        setAlpha = new Color(0, 0, 0, 0);
+        StartCoroutine("Fadeout");
+        Invoke("LoadScene", 3f);
+    }
+
+    IEnumerator Fadeout()
+    {
+        while (curtain.color.a < 255)
+        {
+            setAlpha.a += 0.002f;
+            curtain.color = setAlpha;
+            yield return new WaitForEndOfFrame();
+        }        
+    }
+
+    void LoadScene()
+    {
+        if (true)
+        {
+            SceneManager.LoadScene("GameFrame");
+        }
+        else
+        {
+            SceneManager.LoadScene("Tutorial");
+        }
+        
+        SceneManager.LoadScene("UI", LoadSceneMode.Additive);
+    }
+}
---
+++ DOITGameDev/Assets/Scripts/Stage_BgMove.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stage_BgMove : MonoBehaviour
+{
+    public static float moveSpeed;
+    Vector3 bgSpeed;
+    Transform bgTrans;
+
+    private void Awake()
+    {
+        bgTrans = gameObject.GetComponent<Transform>();
+        bgSpeed = new Vector3(moveSpeed * Time.deltaTime, 0, 0);
+    }
+    
+    void Update()
+    {
+        bgTrans.position -= bgSpeed;
+
+        if (bgTrans.position.x <= -15)
+        {
+            bgTrans.position += new Vector3(30, 0, 0);
+        }
+
+    }
+
+}
