8efa8dc8a72b2a87fe9b715e808c35ebaeb5d2b2
Esoh124 <75177296+Esoh124@users.noreply.github.com>
2021-05-23 20:09:13 +0900

    아이템, 적 수정

--- DOITGameDev/Assets/Scripts/Enemy.cs
+++ DOITGameDev/Assets/Scripts/Enemy.cs
@@ -4,18 +4,23 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-   
+
     public string type;
     Rigidbody2D rigid;
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
-        rigid.velocity = Vector2.left * 0.5f;
+        rigid.velocity = Vector2.left * 1f;
     }
-
-    void meet()
-    {   
-
-        Destroy(gameObject);
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+        if (collision.gameObject.tag == "Border")
+        {
+            Destroy(gameObject);
+        }
     }
 }
